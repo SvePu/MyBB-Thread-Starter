679f6d850777507abaa50ef9601a12229818f04f
SvePu <svp1702@freenet.de>
2015-01-25 18:58:59 +0100

    initial release

---
+++ UPLOAD/inc/languages/english/admin/config_threadstarter.lang.php
@@ -0,0 +1,21 @@
+<?php
+/*
+* ThreadStarter - 1.0 Config Language File English
+* Licensed under GNU/GPL v3
+*/
+
+$l['threadstarter'] = "ThreadStarter";
+$l['threadstarter_desc'] = "Adds a text note or image to the postbit if the postauthor is the threadstarter.";
+$l['threadstarter_settings_title'] = "ThreadStarter Settings";
+$l['threadstarter_settings_title_desc'] = "Settings of ThreadStarter plugin";
+$l['threadstarter_enable_title'] = "Activate ThreadStarter plugin?";
+$l['threadstarter_enable_title_desc'] = "Set YES to enable it!";
+$l['threadstarter_choise_title'] = "What do you want show as thread starter note?";
+$l['threadstarter_choise_title_desc'] = "Select text or image.";
+$l['threadstarter_text_title'] = "Text note for thread starter.";
+$l['threadstarter_text_title_desc'] = "Enter the text you want. (no html allowed!)";
+$l['threadstarter_image_title'] = "Path to thread starter image.";
+$l['threadstarter_image_title_desc'] = "If you want show an image for thread starter, enter the path to that image here. Use <strong>{\$theme}</strong> to specify the image directory for the viewers current theme.";
+$l['threadstarter_firstpostlink_title'] = "Link to first Post?";
+$l['threadstarter_firstpostlink_title_desc'] = "Do you want to link to the first post? (default=YES)";
+?>
---
+++ UPLOAD/inc/plugins/threadstarter.php
@@ -0,0 +1,208 @@
+<?php
+
+/*
+ThreadStarter Plugin for MyBB
+Copyright (C) 2015 SvePu
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+if(!defined("IN_MYBB"))
+{
+	die("Direct initialization of this file is not allowed.<br /><br />Please make sure IN_MYBB is defined.");
+}
+
+$plugins->add_hook('postbit', 'postbit_threadstarter');
+
+function threadstarter_info()
+{
+	global $plugins_cache, $mybb, $db, $lang;
+	$lang->load('config_threadstarter');
+	$info = array
+	(
+		"name"			=>	$db->escape_string($lang->threadstarter),
+		"description"	=>	$db->escape_string($lang->threadstarter_desc),
+		"website"		=>	"https://github.com/SvePu/ThreadStarter",
+		"author"		=>	"SvePu",
+		"authorsite"	=> 	"https://github.com/SvePu",
+		"codename"		=>	"threadstarter",
+		"version"		=>	"1.0",
+		"compatibility"	=>	"18*"
+	);
+	
+	$info_desc = '';
+	$gid_result = $db->simple_select('settinggroups', 'gid', "name = 'threadstarter_settings'", array('limit' => 1));
+	$settings_group = $db->fetch_array($gid_result);
+	if(!empty($settings_group['gid']))
+	{
+		$info_desc .= "<span style=\"font-size: 0.9em;\">(~<a href=\"index.php?module=config-settings&action=change&gid=".$settings_group['gid']."\"> ".$db->escape_string($lang->threadstarter_settings_title)." </a>~)</span>";
+	}
+    
+    if(is_array($plugins_cache) && is_array($plugins_cache['active']) && $plugins_cache['active']['threadstarter'])
+    {
+		$info_desc .= '<form action="https://www.paypal.com/cgi-bin/webscr" method="post" style="float: right;" target="_blank" />
+<input type="hidden" name="cmd" value="_s-xclick" />
+<input type="hidden" name="hosted_button_id" value="VGQ4ZDT8M7WS2" />
+<input type="image" src="https://www.paypalobjects.com/webstatic/en_US/btn/btn_donate_pp_142x27.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" />
+<img alt="" border="0" src="https://www.paypalobjects.com/de_DE/i/scr/pixel.gif" width="1" height="1" />
+</form>';
+	}
+	
+	if($info_desc != '')
+	{
+		$info['description'] = $info_desc.'<br />'.$info['description'];
+	}
+    
+    return $info;
+}
+
+function threadstarter_activate()
+{
+    global $db, $lang;
+	$lang->load('config_threadstarter');
+	$query_add = $db->simple_select("settinggroups", "COUNT(*) as rows");
+	$rows = $db->fetch_field($query_add, "rows");
+    $threadstarter_group = array(
+		"name" 			=>	"threadstarter_settings",
+		"title" 		=>	$db->escape_string($lang->threadstarter_settings_title),
+		"description" 	=>	$db->escape_string($lang->threadstarter_settings_title_desc),
+		"disporder"		=> 	$rows+1,
+		"isdefault" 	=>  0
+	);
+    $db->insert_query("settinggroups", $threadstarter_group);
+	$gid = $db->insert_id();
+	
+	$threadstarter_1 = array(
+        'sid'           => 'NULL',
+        'name'			=> 'threadstarter_enable',
+        'title'			=> $db->escape_string($lang->threadstarter_enable_title),
+        'description'  	=> $db->escape_string($lang->threadstarter_enable_title_desc),
+        'optionscode'  	=> 'yesno',
+        'value'        	=> '1',
+        'disporder'		=> 1,
+        "gid" 			=> (int)$gid
+    );
+	$db->insert_query('settings', $threadstarter_1);
+	
+	$threadstarter_2 = array(
+		"name"			=> "threadstarter_choise",
+		"title"			=> $db->escape_string($lang->threadstarter_choise_title),
+		"description" 	=> $db->escape_string($lang->threadstarter_choise_title_desc),
+        'optionscode'  	=> 'radio \n 1=Text \n 2=Image',
+        'value'        	=> '1',
+		"disporder"		=> 2,
+		"gid" 			=> (int)$gid
+	);
+	$db->insert_query("settings", $threadstarter_2);
+	
+	$threadstarter_3 = array(
+		"name"			=> "threadstarter_text",
+		"title"			=> $db->escape_string($lang->threadstarter_text_title),
+		"description" 	=> $db->escape_string($lang->threadstarter_text_title_desc),
+        'optionscode'  	=> 'text',
+        'value'        	=> '',
+		"disporder"		=> 3,
+		"gid" 			=> (int)$gid
+	);
+	$db->insert_query("settings", $threadstarter_3);
+	
+	
+    $threadstarter_4 = array(
+		"name"			=> "threadstarter_image",
+		"title"			=> $db->escape_string($lang->threadstarter_image_title),
+		"description" 	=> $db->escape_string($lang->threadstarter_image_title_desc),
+        'optionscode'  	=> 'text',
+        'value'        	=> '',
+		"disporder"		=> 4,
+		"gid" 			=> (int)$gid
+	);
+	$db->insert_query("settings", $threadstarter_4);
+	
+	$threadstarter_5 = array(
+		"name"			=> "threadstarter_firstpostlink",
+		"title"			=> $db->escape_string($lang->threadstarter_firstpostlink_title),
+		"description" 	=> $db->escape_string($lang->threadstarter_firstpostlink_title_desc),
+        'optionscode'  	=> 'yesno',
+        'value'        	=> '1',
+		"disporder"		=> 5,
+		"gid" 			=> (int)$gid
+	);
+	$db->insert_query("settings", $threadstarter_5);
+	rebuild_settings();
+	
+	require MYBB_ROOT."/inc/adminfunctions_templates.php";
+	find_replace_templatesets("postbit", "#".preg_quote('{$post[\'userstars\']}')."#i", "{\$post['threadstarter']}\n{\$post['userstars']}");
+	find_replace_templatesets("postbit_classic", "#".preg_quote('{$post[\'userstars\']}')."#i", "{\$post['threadstarter']}\n{\$post['userstars']}");
+}
+
+function threadstarter_deactivate()
+{
+	global $mybb, $db;	
+	
+	$result = $db->simple_select('settinggroups', 'gid', "name = 'threadstarter_settings'", array('limit' => 1));
+	$group = $db->fetch_array($result);
+	
+	if(!empty($group['gid']))
+	{
+		$db->delete_query('settinggroups', "gid='{$group['gid']}'");
+		$db->delete_query('settings', "gid='{$group['gid']}'");
+		rebuild_settings();
+	}
+	
+	require MYBB_ROOT."/inc/adminfunctions_templates.php";
+	find_replace_templatesets("postbit", "#".preg_quote('{$post[\'threadstarter\']}')."(\r?)\n#", '', 0);
+	find_replace_templatesets("postbit_classic", "#".preg_quote('{$post[\'threadstarter\']}')."(\r?)\n#", '', 0);
+}
+
+function postbit_threadstarter(&$post) {
+	global $thread, $mybb, $settings, $postcounter, $theme;	
+	
+	if ($settings['threadstarter_choise'] == 1 && !empty($settings['threadstarter_text'])){
+		$threadstarter = "<span class=\"ts_text\">".htmlspecialchars_uni($settings['threadstarter_text'])."</span>";
+	}
+	else if ($settings['threadstarter_choise'] == 2 && !empty($settings['threadstarter_image'])) 
+	{
+		if (strpos($mybb->settings['threadstarter_image'], '{$theme}') !== false) 
+		{
+			$tsimage = str_replace('{$theme}' , $theme['imgdir'] , $settings['threadstarter_image']);
+		} 
+		else 
+		{
+			$tsimage = $settings['threadstarter_image'];
+		}
+		list($width, $height, $type, $width_height) = getimagesize($tsimage);
+		$threadstarter = "<img class=\"ts_image\" ".$width_height." src=\"".$tsimage."\" alt=\"threadstarter\" />";
+	}
+	else
+	{
+		$threadstarter = "<span class=\"ts_text\">ThreadStarter</span>";
+	}
+	
+	if ($settings['threadstarter_enable'] != 0) 
+	{	
+		$post['threadstarter'] = "";
+		if ($post['uid'] == $thread ['uid'] && $postcounter != 1) 
+		{
+			if($mybb->settings['threadstarter_firstpostlink'] != 0)
+			{
+			$post['threadstarter'] = "<a href=\"".$settings['bburl']."/".get_thread_link($thread['tid'])."\">".$threadstarter."</a><br />";
+			}
+			else
+			{
+			$post['threadstarter'] = $threadstarter."<br />";
+			}
+		}
+	
+	}
+}
